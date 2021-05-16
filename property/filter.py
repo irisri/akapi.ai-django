e1ab35583fa1af978f13e32a4c56c15e814222a3
Iris Rifold <irisrifold@gmail.com>
2021-05-16 21:20:59 +0300

    cleanup

--- property/filter.py
+++ property/filter.py
@@ -4,7 +4,6 @@ from .models import Property
 
 
 class SqftFilter(django_filters.FilterSet):
-    # sqft = django_filters.NumberFilter(field_name='unit_sqft', lookup_expr='gt')
     class Meta:
         model = Property
         fields =['unit_sqft']
